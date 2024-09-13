357737ab4de4a261b596b1d9cbeddf4b12913788
Saad Ahmad <saadahmad6830879@gmail.com>
2024-09-13 23:06:01 +0500

    almost finalized with videos

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
 	// typing text animation script
 	var typed = new Typed(".typing", {
-		strings: ["Video Editor", "Graphics Designer", "Freelancer"],
+		strings: ["like Pro", "with me"],
 		typeSpeed: 100,
 		backSpeed: 60,
 		loop: true,
