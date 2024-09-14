a222971cf517f91ee974a99a6a3abbac93dd7b31
Saad Ahmad <saadahmad6830879@gmail.com>
2024-09-14 19:33:15 +0500

    final revision

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
 	// typing text animation script
 	var typed = new Typed(".typing", {
-		strings: ["like Pro", "with us"],
+		strings: ["like Pro", "with me"],
 		typeSpeed: 100,
 		backSpeed: 60,
 		loop: true,
@@ -94,15 +94,22 @@ $(document).ready(function () {
 	});
 });
 
+////////////////////////
 
 // // Get modal and iframe elements
 // const videoModal = document.getElementById('video-modal');
 // const videoIframe = document.getElementById('video-iframe');
+// const videoModalContent = document.getElementById('video-modal-content');
 
 // // Define video IDs and corresponding thumbnail IDs
 // const videoIds = {
-//   'video-1-thumbnail': '1DmZwEnF5cxadz15-PyntnzmY4ZkV7ipE',
-//   'video-2-thumbnail': '1bWzT4bqT5O4jUMHZXNEPtP9uxmdxVs2X',
+//   'video-1-thumbnail': '1STWlh1BLSrZLa0IMWT6U2P5OkT9nwbb9',
+//   'video-2-thumbnail': '1IkJg_YWiiBMQtavP7KkCd9KyPXShRrMj',
+//   'video-3-thumbnail': '1OQPbwZQVe4cXI942hbx5-sQQQ0H3ZpJF',
+//   'video-4-thumbnail': '1DmZwEnF5cxadz15-PyntnzmY4ZkV7ipE',
+//   'video-5-thumbnail': '1pAjBITzm-gHSoARo5zRccYNHVkw_bnZb',
+//   'video-6-thumbnail': '1-2R3RmgO1J2F6whlV-f4TOIOzUKzYBDJ',
+//   'video-7-thumbnail': '1opT-Ywk7hzn8gjRmy_9BLDIFn1rVtgjR',
 //   // Add more video IDs here
 // };
 
@@ -120,10 +127,10 @@ $(document).ready(function () {
 //   });
 // });
 
-// // Add event listener to modal close button
-// document.getElementById('video-modal').addEventListener('click', (e) => {
-//   // Check if the click is on the modal background
-//   if (e.target.id === 'video-modal') {
+// // Add event listener to modal background and close button
+// videoModal.addEventListener('click', (e) => {
+//   // Check if the click is on the modal background or close button
+//   if (e.target.id === 'video-modal' || e.target.id === 'video-modal-content') {
 //     // Reset the iframe source
 //     videoIframe.src = '';
     
@@ -132,45 +139,57 @@ $(document).ready(function () {
 //   }
 // });
 
+////////////////////////
 // Get modal and iframe elements
-const videoModal = document.getElementById('video-modal');
-const videoIframe = document.getElementById('video-iframe');
-const videoModalContent = document.getElementById('video-modal-content');
+// const videoModal = document.getElementById('video-modal');
+// const videoIframe = document.getElementById('video-iframe');
 
 // Define video IDs and corresponding thumbnail IDs
 const videoIds = {
-  'video-1-thumbnail': '1STWlh1BLSrZLa0IMWT6U2P5OkT9nwbb9',
-  'video-2-thumbnail': '1IkJg_YWiiBMQtavP7KkCd9KyPXShRrMj',
-  'video-3-thumbnail': '1OQPbwZQVe4cXI942hbx5-sQQQ0H3ZpJF',
-  'video-4-thumbnail': '1DmZwEnF5cxadz15-PyntnzmY4ZkV7ipE',
-  'video-5-thumbnail': '1pAjBITzm-gHSoARo5zRccYNHVkw_bnZb',
-  'video-6-thumbnail': '1-2R3RmgO1J2F6whlV-f4TOIOzUKzYBDJ',
-  'video-7-thumbnail': '1opT-Ywk7hzn8gjRmy_9BLDIFn1rVtgjR',
-  // Add more video IDs here
+    'video-1-thumbnail': '1r5h2HUOkS5IVXHEKgLgejMggFvx9UrQ0',
+    'video-2-thumbnail': '1IkJg_YWiiBMQtavP7KkCd9KyPXShRrMj',
+    'video-3-thumbnail': '1OQPbwZQVe4cXI942hbx5-sQQQ0H3ZpJF',
+    'video-4-thumbnail': '1DmZwEnF5cxadz15-PyntnzmY4ZkV7ipE',
+    'video-5-thumbnail': '1pAjBITzm-gHSoARo5zRccYNHVkw_bnZb',
+    'video-6-thumbnail': '1-2R3RmgO1J2F6whlV-f4TOIOzUKzYBDJ',
+    'video-7-thumbnail': '1opT-Ywk7hzn8gjRmy_9BLDIFn1rVtgjR',
+    // Add more video IDs here
 };
 
+// // Add event listeners to video thumbnails
+// document.querySelectorAll('#video-thumbnails img').forEach((thumbnail) => {
+//     thumbnail.addEventListener('click', (e) => {
+//         // Get the video ID from the thumbnail ID
+//         const videoId = videoIds[e.target.id];
+        
+//         // Set the iframe source to the corresponding video URL
+//         videoIframe.src = `https://drive.google.com/file/d/${videoId}/preview`; // Use 'preview' instead of 'view'
+        
+//         // Show the modal
+//         videoModal.style.display = 'block'; // Remove '!important'
+//     });
+// });
+
+// // Add event listener to modal background to close it
+// videoModal.addEventListener('click', (e) => {
+//     // Check if the click is on the modal background
+//     if (e.target.id === 'video-modal') {
+//         // Reset the iframe source
+//         videoIframe.src = '';
+        
+//         // Hide the modal
+//         videoModal.style.display = 'none';
+//     }
+// });
+
+//// new code for new tab///
 // Add event listeners to video thumbnails
 document.querySelectorAll('#video-thumbnails img').forEach((thumbnail) => {
-  thumbnail.addEventListener('click', (e) => {
-    // Get the video ID from the thumbnail ID
-    const videoId = videoIds[e.target.id];
-    
-    // Set the iframe source to the corresponding video URL
-    videoIframe.src = `https://drive.google.com/file/d/${videoId}/preview`;
-    
-    // Show the modal
-    videoModal.style.display = 'block';
-  });
-});
+	thumbnail.addEventListener('click', (e) => {
+		// Get the video ID from the thumbnail ID
+		const videoId = videoIds[e.target.id];
 
-// Add event listener to modal background and close button
-videoModal.addEventListener('click', (e) => {
-  // Check if the click is on the modal background or close button
-  if (e.target.id === 'video-modal' || e.target.id === 'video-modal-content') {
-    // Reset the iframe source
-    videoIframe.src = '';
-    
-    // Hide the modal
-    videoModal.style.display = 'none';
-  }
+		// Open the Google Drive link in a new tab
+		window.open(`https://drive.google.com/file/d/${videoId}/view`, '_blank');
+	});
 });
