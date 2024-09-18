f051f2baa203973dbe27aa1ce0f1ca6f8a4b7803
Saad Ahmad <saadahmad6830879@gmail.com>
2024-09-18 08:35:25 +0500

    revision after finallzz

--- script.js
+++ script.js
@@ -146,13 +146,17 @@ $(document).ready(function () {
 
 // Define video IDs and corresponding thumbnail IDs
 const videoIds = {
-    'video-1-thumbnail': '1r5h2HUOkS5IVXHEKgLgejMggFvx9UrQ0',
+    'video-1-thumbnail': '1STWlh1BLSrZLa0IMWT6U2P5OkT9nwbb9',
     'video-2-thumbnail': '1IkJg_YWiiBMQtavP7KkCd9KyPXShRrMj',
     'video-3-thumbnail': '1OQPbwZQVe4cXI942hbx5-sQQQ0H3ZpJF',
     'video-4-thumbnail': '1DmZwEnF5cxadz15-PyntnzmY4ZkV7ipE',
     'video-5-thumbnail': '1pAjBITzm-gHSoARo5zRccYNHVkw_bnZb',
     'video-6-thumbnail': '1-2R3RmgO1J2F6whlV-f4TOIOzUKzYBDJ',
     'video-7-thumbnail': '1opT-Ywk7hzn8gjRmy_9BLDIFn1rVtgjR',
+	'video-8-thumbnail': '1mGNrnPvUFJGkVbeT4AG5aVms7CPDSvhO',
+	'video-9-thumbnail': '1rJeWTHIJEQt-A1eGBhEKlJC4vviZ-uQU',	
+	'video-10-thumbnail': '1_7eNZ5VRNqYNiYiqfx9iQZpeRHcbLvOc',
+	
     // Add more video IDs here
 };
 
@@ -184,7 +188,7 @@ const videoIds = {
 
 //// new code for new tab///
 // Add event listeners to video thumbnails
-document.querySelectorAll('#video-thumbnails img').forEach((thumbnail) => {
+document.querySelectorAll('.thumbnail-container img').forEach((thumbnail) => {
 	thumbnail.addEventListener('click', (e) => {
 		// Get the video ID from the thumbnail ID
 		const videoId = videoIds[e.target.id];
@@ -192,4 +196,24 @@ document.querySelectorAll('#video-thumbnails img').forEach((thumbnail) => {
 		// Open the Google Drive link in a new tab
 		window.open(`https://drive.google.com/file/d/${videoId}/view`, '_blank');
 	});
+});
+
+
+////////////////////////
+
+
+const thumbnails = document.querySelectorAll('.thumbnail-container img');
+
+thumbnails.forEach((thumbnail) => {
+    thumbnail.addEventListener('mouseenter', (e) => {
+        const textElement = e.target.nextElementSibling;
+        textElement.style.visibility = 'visible';
+        textElement.style.opacity = '1';
+    });
+
+    thumbnail.addEventListener('mouseleave', (e) => {
+        const textElement = e.target.nextElementSibling;
+        textElement.style.visibility = 'hidden';
+        textElement.style.opacity = '0';
+    });
 });
